d3bdcc1becf1f9cf7a9963487bbb8d49d4170275
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-19 15:24:34 +0530

    first commit

---
+++ multi-line comments.js
@@ -0,0 +1,4 @@
+/* below is the console
+that we use to
+print in the console on browser */
+console.log("hello Sir") 
---
+++ single-line comment.js
@@ -0,0 +1,3 @@
+// this is the single line Comment
+// so this is that how we comment single line
+console.log("hello sir");
